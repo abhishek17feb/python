36ec28417d667464a17dc7eb266338392c03fa85
abhishek2097 <abhishek2097@gmail.com>
2020-01-12 21:38:35 +0530

    Numbers

---
+++ Python Object and Data Structure Basics/numbers.py
@@ -0,0 +1,15 @@
+addition = 1+2
+substraction = 31 - 78
+division = 20 / 6
+multiplication = 7*5
+
+print('Addition example',addition)
+print('Substraction example',substraction)
+print('Division example',division)
+print('Multplication example',multiplication)
+
+remainder = 20 % 2
+print('Remainder is',remainder)
+
+powerOfTwo = 2 ** 2
+print('2 power 2 is',powerOfTwo)
