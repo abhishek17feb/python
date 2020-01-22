1c3fe5ee63753eacfe5e5aa8fe809edf713e290a
Abhishek.Roy <abhishek2097@gmail.com>
2020-01-22 07:21:39 +0530

    Tuple examples

---
+++ Python Object and Data Structure Basics/tuple.py
@@ -0,0 +1,8 @@
+##Tuples are similar to list with a difference of immutability. That means once a tuple is made, the objects at
+## any index position cannot be changed. Again tuples can hold mixure of data types
+
+# E.g myTuple = ('abhishek',450)
+tuple = ('abhishek',345,678)
+print(tuple[0]) #abhishek
+print(tuple[-1]) #678
+
