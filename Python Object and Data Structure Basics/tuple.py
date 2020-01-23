b7fa940a5bd02ce4a6891f34255efc55eaf8be7c
Abhishek.Roy <abhishek2097@gmail.com>
2020-01-23 14:43:26 +0530

    Tuple examples

--- Python Object and Data Structure Basics/tuple.py
+++ Python Object and Data Structure Basics/tuple.py
@@ -6,3 +6,20 @@ tuple = ('abhishek',345,678)
 print(tuple[0]) #abhishek
 print(tuple[-1]) #678
 
+##Tuples have 2 methods i.e count and index
+countChars = ('a','f','a')
+print(countChars.count('a'))#2
+
+print(countChars.index('a')) #0 The first index location of occurance
+
+## Showing how list are different from a tuple
+myList = [1,2,3]
+myList[2] = 8 #Item reassignment is possible with list
+print(myList)
+
+countChars[1] = 4 #    countChars[1] = 4
+#TypeError: 'tuple' object does not support item assignment
+print(countChars)
+
+##So tuples are used when data integrity is required as you cannot re-assign something in tuple accidently
+
